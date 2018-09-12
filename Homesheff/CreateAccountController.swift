a0ea3a69d15bfe09448aac76a1ed57f1189cf480
Dimitrios Papageorgiou <dmpapageo@gmail.com>
2018-09-12 15:56:40 -0400

    Initial commit. Initiated designs of splash, login, and create your account screens

---
+++ Homesheff/CreateAccountController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateYourAccountController.swift
+//  Homesheff
+//
+//  Created by Dimitrios Papageorgiou on 9/12/18.
+//  Copyright © 2018 Dimitrios Papageorgiou. All rights reserved.
+//
+
+
+import UIKit
+import Foundation
+
+//extension UITextField{
+//    
+//    func setPadding(){
+//        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+//        self.leftView = paddingView
+//        self.leftViewMode = .always
+//    }
+//    func bottomBorder(){
+//        self.layer.shadowColor = UIColor.white.cgColor
+//        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+//        self.layer.shadowOpacity = 1.0
+//        self.layer.shadowRadius = 0.0
+//    }
+//}
--- Homesheff/ViewController.swift
+++ Homesheff/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
+extension UITextField{
+    
+    func setPadding(){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    func bottomBorder(){
+        self.layer.shadowColor = UIColor.white.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+    }
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        usernameTextField.setPadding()
+        usernameTextField.bottomBorder()
+        
+        passwordTextField.setPadding()
+        passwordTextField.bottomBorder()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +45,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
