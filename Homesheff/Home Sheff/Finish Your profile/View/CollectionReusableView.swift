a989bfea8b03e94b903b231d555f4e406e980400
Anurag Yadav <ang.ydv@gmail.com>
2018-10-13 18:02:33 -0400

    UI modification for Sign up svcreen

---
+++ Homesheff/Home Sheff/Finish Your profile/View/CollectionReusableView.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionReusableView.swift
+//  Homesheff
+//
+//  Created by Anurag Yadev on 10/10/18.
+//  Copyright © 2018 Dimitrios Papageorgiou. All rights reserved.
+//
+
+import UIKit
+
+class CollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ Homesheff/Home Sheff/Finish Your profile/View/SelectedServiceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelectedServiceTableViewCell.swift
+//  Homesheff
+//
+//  Created by Anurag Yadev on 10/10/18.
+//  Copyright © 2018 Dimitrios Papageorgiou. All rights reserved.
+//
+
+import UIKit
+
+class SelectedServiceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
