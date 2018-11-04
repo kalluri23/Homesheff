1853974e69c59a36b3c6e6dfc35fc7a8ec529d3d
Anurag Pandey <anurag.pandey@accuweather.com>
2018-11-04 17:28:52 -0500

    Added placeholder Image Url for Chef

--- Homesheff/Home Sheff/Cheif Finder/View/ChefCell.swift
+++ Homesheff/Home Sheff/Cheif Finder/View/ChefCell.swift
@@ -14,12 +14,12 @@ class ChefCell: UITableViewCell {
 
     @IBOutlet weak var chefName: UILabel!
     @IBOutlet weak var chefLocation: UILabel!
-    
     @IBOutlet weak var chefRate: UILabel!
+    private let placeholderImageUrl = "https://png.icons8.com/color/2x/person-female.png"
     
     var chef: Chef? {
         didSet{
-            cheffImageView.loadImageWithUrlString(urlString: chef?.imageURL ?? "")
+            cheffImageView.loadImageWithUrlString(urlString: chef?.imageURL ?? placeholderImageUrl)
             chefName.text = chef?.firstName
             chefLocation.text = "Virginia, USA"
             chefRate.text = ""
